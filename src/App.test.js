ca8a8ffdf9b68404c2a347b227642b1a83e2b0a9
Enzo <enzomatheus.df@gmail.com>
2026-05-31 08:04:28 -0300

    Deploy site

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app loading screen', () => {
-  render(<App />);
-  const loadingElement = screen.getByText(/Carregando perfil/i);
-  expect(loadingElement).toBeInTheDocument();
-});
